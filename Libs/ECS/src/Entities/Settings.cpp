38037d349847d944e38e9d6f27b91a6c42ac8d1b
Hope <114905446+anthonyhuhuee@users.noreply.github.com>
2024-10-07 15:49:05 +0200

    fix : update Settings.cpp

--- Libs/ECS/src/Entities/Settings.cpp
+++ Libs/ECS/src/Entities/Settings.cpp
@@ -9,11 +9,8 @@
 
 namespace ECS {
     void settingsHandler(Coordinator &gCoordinator, std::uint32_t entity) {
-        std::cout << "Step 1" << std::endl;
         gCoordinator.addComponent(entity, Keybind());
-        std::cout << "Step 2" << std::endl;
         //gCoordinator.addComponent(entity, Sounds{LoadSound(""), 50, "Background Music" });
-        std::cout << "Step 3" << std::endl;
         Keybind& keybindComponent = gCoordinator.getComponent<Keybind>(entity);
         keybindComponent.addKeybind(KEY_UP, "Up", "Move Up");
         keybindComponent.addKeybind(KEY_DOWN, "Down", "Move Down");
@@ -21,7 +18,6 @@ namespace ECS {
         keybindComponent.addKeybind(KEY_RIGHT, "Right", "Move Right");
         keybindComponent.addKeybind(KEY_SPACE, "Shoot", "Shoot/Select");
         keybindComponent.addKeybind(KEY_ESCAPE, "Pause", "Pause/Unpause");
-        std::cout << "Step 4" << std::endl;
         gCoordinator.setEntityInitialized(entity, true);
     }
 }
